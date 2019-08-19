8fa456ef87f17cbc3c5108cc2b7506cdbc4ba8df
Ask149 <ashishkshirsagar10@gmail.com>
2019-08-19 23:48:02 +0530

    Integrate tests for GANs

---
+++ tmva/tmva/test/DNN/CNN/TestMethodGAN.cxx
@@ -0,0 +1,45 @@
+// @(#)root/tmva/tmva/cnn:$Id$
+// Author: Anushree Rankawat
+
+/**********************************************************************************
+ * Project: TMVA - a Root-integrated toolkit for multivariate data analysis       *
+ * Package: TMVA                                                                  *
+ * Class  :                                                                       *
+ * Web    : http://tmva.sourceforge.net                                           *
+ *                                                                                *
+ * Description:                                                                   *
+ *      Testing Method GAN                                                        *
+ *                                                                                *
+ * Authors (alphabetical):                                                        *
+ *      Anushree Rankawat <anushreerankawat110@gmail.com>                         *
+ *                                                                                *
+ * Copyright (c) 2005-2015:                                                       *
+ *      CERN, Switzerland                                                         *
+ *      U. of Victoria, Canada                                                    *
+ *      MPI-K Heidelberg, Germany                                                 *
+ *      U. of Bonn, Germany                                                       *
+ *                                                                                *
+ * Redistribution and use in source and binary forms, with or without             *
+ * modification, are permitted according to the terms listed in LICENSE           *
+ * (http://tmva.sourceforge.net/LICENSE)                                          *
+ **********************************************************************************/
+
+#include "TestMethodGAN.h"
+#include "TString.h"
+
+int main()
+{
+   std::cout << "Testing Method GAN for CPU backend: " << std::endl;
+
+   TString archCPU = "CPU";
+
+   testMethodGAN_DNN(archCPU);
+   std::cout << "Creating Noisy Matrix " << std::endl;   
+   testCreateNoisyMatrices();
+   std::cout << "Creating Discriminator Fake Data " << std::endl;
+   testCreateDiscriminatorFakeData();
+   std::cout << "Calling CombineGAN " << std::endl;
+   testCombineGAN();
+   std::cout << "Finished " << std::endl;
+
+}
